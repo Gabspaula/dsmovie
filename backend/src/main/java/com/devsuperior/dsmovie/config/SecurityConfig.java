899e52d6e2785a3b252c169192c6922fcf5a4728
Gabspaula <gaabi.silvestre@gmail.com>
2022-03-13 19:19:52 -0300

    Domain model, database seed

--- backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.devsuperior.dsmovie.config;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
